d8e7627aa0f62a03d9d1237abe28217e0d0eb329
Younis Shah <younisashah@gmail.com>
2023-03-20 19:12:02 +0800

    feat(sh-files): adds default value to last succesful commit

--- print_affected.sh
+++ print_affected.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-echo "[affected]: PREVIOUS_SUCCESSFUL_COMMIT: ${PREVIOUS_SUCCESSFUL_COMMIT:-HEAD~1}";
+PREVIOUS_SUCCESSFUL_COMMIT=${PREVIOUS_SUCCESSFUL_COMMIT:-HEAD~1}
+echo "[affected]: PREVIOUS_SUCCESSFUL_COMMIT: ${PREVIOUS_SUCCESSFUL_COMMIT}";
 
 npx nx print-affected --targets=install:ci,build --base="$PREVIOUS_SUCCESSFUL_COMMIT" --head=HEAD --select=projects
