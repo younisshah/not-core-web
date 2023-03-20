6e95316bb1d5e60bab496783307cb36ee3449a00
Younis Shah <younisashah@gmail.com>
2023-03-20 19:07:32 +0800

    feat(Jenkinsfile): adds default base

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-echo "[build]: PREVIOUS_SUCCESSFUL_COMMIT: ${PREVIOUS_SUCCESSFUL_COMMIT}";
+echo "[build]: PREVIOUS_SUCCESSFUL_COMMIT: ${PREVIOUS_SUCCESSFUL_COMMIT:-HEAD~1}";
 
 npx nx affected --targets=install:ci,build --base="$PREVIOUS_SUCCESSFUL_COMMIT" --head=HEAD
 
--- print_affected.sh
+++ print_affected.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-echo "[affected]: PREVIOUS_SUCCESSFUL_COMMIT: ${PREVIOUS_SUCCESSFUL_COMMIT}";
+echo "[affected]: PREVIOUS_SUCCESSFUL_COMMIT: ${PREVIOUS_SUCCESSFUL_COMMIT:-HEAD~1}";
 
 npx nx print-affected --targets=install:ci,build --base="$PREVIOUS_SUCCESSFUL_COMMIT" --head=HEAD --select=projects
