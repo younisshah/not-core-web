ed29a08f274008fe13660960a01d49789d503869
Younis Shah <younisashah@gmail.com>
2023-03-21 11:19:42 +0800

    feat(prepare): installs nx globally

--- prepare.sh
+++ prepare.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-npm install -g lerna@6.5.1 nx@15.8.7
+npm install -g nx@15.8.7
 
-NODE_OPTIONS=--max_old_space_size=8192 lerna bootstrap --hoist
+NODE_OPTIONS=--max_old_space_size=8192 npx lerna bootstrap --hoist
