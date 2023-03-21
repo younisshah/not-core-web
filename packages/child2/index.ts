7f57d1d8f25a4fc766a2de36b635dfb94fc4e3a4
Younis Shah <younisashah@gmail.com>
2023-03-21 15:55:21 +0800

    feat(child2): adds 42 as the answer

--- packages/child2/index.ts
+++ packages/child2/index.ts
@@ -3,6 +3,5 @@ import { log } from '../shared';
 export function bazQux(msg = 'default') {
   log(`Running second child app with message: ${msg}`);
 
-  const answer = 42;
-  console.log(`The answer is ${answer}`);
+  console.log(`The answer is 42`);
 }
