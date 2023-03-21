5ac4feea38d4b8334f5b2a1eefea304b7a49e1e3
Younis Shah <younisashah@gmail.com>
2023-03-21 11:21:33 +0800

    feat(child2): prints answer

--- packages/child2/index.ts
+++ packages/child2/index.ts
@@ -2,5 +2,7 @@ import { log } from '../shared';
 
 export function bazQux(msg = 'default') {
   log(`Running second child app with message: ${msg}`);
-  console.log(`42 * 2 = ${42 * 2}`);
+
+  const answer = 42;
+  console.log(`The answer is ${answer}`);
 }
