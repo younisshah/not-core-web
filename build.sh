7693c1b224563cf1599dd702bb87b583fad4ab1e
Younis Shah <younisashah@gmail.com>
2023-03-26 15:22:57 +0800

    feat(build.sh): updates build.sh to add affected_widgets

--- build.sh
+++ build.sh
@@ -2,6 +2,16 @@
 
 echo "[build]: PREVIOUS_SUCCESSFUL_COMMIT: ${PREVIOUS_SUCCESSFUL_COMMIT}";
 
-npx nx affected --target=build --base="$PREVIOUS_SUCCESSFUL_COMMIT" --head=HEAD
+affected_widgets=$(npx nx print-affected --base="$PREVIOUS_SUCCESSFUL_COMMIT" --head=HEAD --target=build --select=projects)
 
+if [ -z "$affected_widgets" ]; then
+    echo "[build]: No affected widgets"
+    echo "[build]: Building lib..."
+    npx nx run lib:build
+else
+    echo "[build]: Affected widgets: $affected_widgets"
+fi
+
+
+npx nx affected --target=build --base="$PREVIOUS_SUCCESSFUL_COMMIT" --head=HEAD
 node ./ci/publish_apps.js
